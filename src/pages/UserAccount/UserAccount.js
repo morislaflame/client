36e744a99d8ae390bfb25fec2e8d2e6e4bfb9571
morislaflame <andrey.uraltender@yandex.ru>
2024-09-11 19:45:39 +0500

    UserAccount

--- src/pages/UserAccount/UserAccount.js
+++ src/pages/UserAccount/UserAccount.js
@@ -51,6 +51,7 @@ const UserAccount = observer(() => {
                                     </li>
                                 ))}
                             </ul>
+                            Общая стоимость: {order.totalPrice}
                         </ListGroup.Item>
                     ))}
                 </ListGroup>
