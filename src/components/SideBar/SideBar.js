95eb3a728070cd4a1ec342fcad9c53865e52809c
morislaflame <andrey.uraltender@yandex.ru>
2024-10-19 19:45:12 +0500

    BeforeButton

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,44 +1,48 @@
-import React from 'react'
-import TypeBar from '../TypeBar/TypeBar'
-import BrandBar from '../BrandBar/BrandBar'
+import React from 'react';
+import { observer } from 'mobx-react-lite';
+import TypeBar from '../TypeBar/TypeBar';
+import BrandBar from '../BrandBar/BrandBar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import MyButton from '../MyButton/MyButton';
-import './SideBar.css'
+import './SideBar.css';
 import PriceSlider from '../PriceSlider/PriceSlider';
+import SelectedFilters from '../SelectedFilters/SelectedFilters';
+import { Context } from '../../index';
 
-export default function SideBar({ name, ...props }) {
-
+const SideBar = observer(({ name, ...props }) => {
     const [show, setShow] = useState(false);
+    const { thing } = useContext(Context);
 
-  const handleClose = () => setShow(false);
-  const toggleShow = () => setShow((s) => !s);
+    const handleClose = () => setShow(false);
+    const toggleShow = () => setShow((s) => !s);
 
-  return (
-
-    <>
-      <MyButton text="Filters" onClick={toggleShow}/>
-      <Offcanvas show={show} onHide={handleClose} scroll
-      backdrop placement="top">
-        <Offcanvas.Header>
-          <Offcanvas.Title>FILTERS</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-            <div className={'sidebar'}>
-                <div>
-                  <PriceSlider/>
-                </div>
-                <div>
-                    <BrandBar/>
-                </div>
-                <div>
-                    <TypeBar/>
-                </div>
+    return (
+        <>
+            <div className="sidebar-header">
+                <MyButton text="Filters" onClick={toggleShow} />
+                <SelectedFilters />
             </div>
-        </Offcanvas.Body>
-      </Offcanvas>
-    </>
-    
-    
-  )
-}
+            <Offcanvas show={show} onHide={handleClose} scroll backdrop placement="top">
+                <Offcanvas.Header>
+                    <Offcanvas.Title>FILTERS</Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                    <div className={'sidebar'}>
+                        <div>
+                            <PriceSlider />
+                        </div>
+                        <div>
+                            <BrandBar />
+                        </div>
+                        <div>
+                            <TypeBar />
+                        </div>
+                    </div>
+                </Offcanvas.Body>
+            </Offcanvas>
+        </>
+    );
+});
+
+export default SideBar;
