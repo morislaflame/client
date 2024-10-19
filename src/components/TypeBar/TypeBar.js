3202d29245e0b84752a918243c0fc4031ec6eca4
morislaflame <andrey.uraltender@yandex.ru>
2024-10-19 22:57:07 +0500

    Filters

--- src/components/TypeBar/TypeBar.js
+++ src/components/TypeBar/TypeBar.js
@@ -1,67 +1,40 @@
 import { observer } from "mobx-react-lite";
 import React, { useContext, useState } from "react";
 import { Context } from "../../index";
-import Form from 'react-bootstrap/Form';
-import ListGroup from 'react-bootstrap/ListGroup';
+import { AutoComplete } from 'antd';
 import './TypeBar.css';
 
 const TypeBar = observer(() => {
     const { thing } = useContext(Context);
     const [searchTerm, setSearchTerm] = useState("");
 
-    const firstThreeTypes = thing.types.slice(0, 3);
+    const options = thing.types.map(type => ({
+        value: type.name,
+        label: type.name
+    }));
 
-    // Фильтруем типы по поисковому запросу, но показываем их только если есть ввод
-    const filteredTypes = thing.types.filter(type =>
-        type.name.toLowerCase().includes(searchTerm.toLowerCase())
-    ).slice(0, 5); // Показываем не более 5 результатов
-
-    const handleTypeClick = (type) => {
-        if (thing.selectedType.id === type.id) {
-            thing.setSelectedType({});
-        } else {
-            thing.setSelectedType(type);
+    const handleSelect = (value) => {
+        const selectedType = thing.types.find(type => type.name === value);
+        if (selectedType) {
+            thing.setSelectedType(selectedType);
         }
     };
 
     return (
         <div className="typebar">
-            <ListGroup>
-                {firstThreeTypes.map(type => (
-                    <ListGroup.Item
-                        className={`type_name ${type.id === thing.selectedType.id ? 'selected' : ''}`}
-                        onClick={() => handleTypeClick(type)}
-                        key={type.id}
-                        action
-                    >
-                        {type.name}
-                    </ListGroup.Item>
-                ))}
-            </ListGroup>
-
-            <Form.Group className="mt-3">
-                <Form.Control
-                    type="text"
-                    placeholder="Search by country"
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                />
-            </Form.Group>
-
-            {searchTerm && (
-                <ListGroup className="mt-2">
-                    {filteredTypes.map(type => (
-                        <ListGroup.Item
-                            className={`type_name ${type.id === thing.selectedType.id ? 'selected' : ''}`}
-                            onClick={() => handleTypeClick(type)}
-                            key={type.id}
-                            action
-                        >
-                            {type.name}
-                        </ListGroup.Item>
-                    ))}
-                </ListGroup>
-            )}
+            <AutoComplete
+                style={{ width: '100%' }}
+                options={options}
+                value={searchTerm}
+                onChange={setSearchTerm}
+                onSelect={handleSelect}
+                placeholder="Country"
+            />
+            {/* {thing.selectedType.id && (
+                <div className="selected-type">
+                    <span className="type_name">{thing.selectedType.name}</span>
+                </div>
+            )} */}
         </div>
     );
 });
