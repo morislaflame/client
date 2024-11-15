23afb7c90fe30ebf9f86b5adaf9b9b86e7b49d54
morislaflame <andrey.uraltender@yandex.ru>
2024-11-15 17:18:15 +0500

    navbar

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,34 +1,129 @@
-import React from 'react';
-import { Drawer } from 'antd';
+import React, { useContext } from 'react';
+import { Drawer, Button } from 'antd';
 import styles from './Menu.module.css';
-import { USER_ACCOUNT_ROUTE, SELLER_ACCOUNT_ROUTE } from '../../utils/consts';
-import { NavLink } from 'react-router-dom';
+import { Context } from '../../index';
+import {
+  USER_ACCOUNT_ROUTE,
+  SELLER_ACCOUNT_ROUTE,
+  SHOP_ROUTE,
+  PRIVACY_ROUTE,
+  TERMS_ROUTE,
+  ADMIN_ROUTE,
+  LOGIN_ROUTE,
+} from '../../utils/consts';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { FaUserAlt } from 'react-icons/fa';
-import { AiFillDollarCircle } from "react-icons/ai";
+import { AiFillDollarCircle } from 'react-icons/ai';
+import { FaTelegram } from 'react-icons/fa';
+import { FaCartShopping } from 'react-icons/fa6';
+import { FaUsers } from 'react-icons/fa';
+import { IoShieldCheckmarkSharp } from 'react-icons/io5';
 
 const Menu = ({ open, onClose }) => {
-    return (
-        <Drawer title="Menu" onClose={onClose} open={open} width="auto">
-            <h3>Navigation</h3>
-            <div className={styles.links_container}>
-                <NavLink
-                    to={USER_ACCOUNT_ROUTE}
-                    onClick={onClose}
-                    className={styles.menuLink}
-                >
-                   <FaUserAlt /> My Account
-                </NavLink>
-                <NavLink
-                    to={SELLER_ACCOUNT_ROUTE}
-                    onClick={onClose}
-                    className={styles.menuLink}
-                >
-                    <AiFillDollarCircle /> Seller Account
-                </NavLink>
-            </div>
-            {/* Добавьте другие пункты меню по необходимости */}
-        </Drawer>
-    );
+  const { user } = useContext(Context);
+  const navigate = useNavigate();
+
+  const logOut = () => {
+    user.logout();
+    if (window.Telegram?.WebApp?.HapticFeedback) {
+      window.Telegram.WebApp.HapticFeedback.impactOccurred('heavy');
+    }
+    navigate(LOGIN_ROUTE);
+    onClose(); // Закрываем Drawer после выхода
+  };
+
+  return (
+    <Drawer title="Menu" onClose={onClose} open={open} width="auto">
+      <h3>Navigation</h3>
+      <div className={styles.links_container}>
+        {user.isAuth ? (
+          <>
+            <NavLink
+              to={USER_ACCOUNT_ROUTE}
+              onClick={onClose}
+              className={styles.menuLink}
+            >
+              <FaUserAlt /> My Account
+            </NavLink>
+            {user.user.role === 'SELLER' && (
+              <NavLink
+                to={SELLER_ACCOUNT_ROUTE}
+                onClick={onClose}
+                className={styles.menuLink}
+              >
+                <AiFillDollarCircle /> Seller Account
+              </NavLink>
+            )}
+            {user.user.role === 'ADMIN' && (
+              <Button
+                type="primary"
+                block
+                onClick={() => {
+                  navigate(ADMIN_ROUTE);
+                  onClose();
+                }}
+                style={{ marginTop: '8px' }}
+              >
+                Админка
+              </Button>
+            )}
+            <Button
+              type="primary"
+              danger
+              block
+              onClick={logOut}
+              style={{ marginTop: '8px' }}
+            >
+              Выйти
+            </Button>
+          </>
+        ) : (
+          <Button
+            type="primary"
+            block
+            onClick={() => {
+              navigate(LOGIN_ROUTE);
+              onClose();
+            }}
+            style={{ marginTop: '8px' }}
+          >
+            Авторизация
+          </Button>
+        )}
+        {/* Другие ссылки */}
+        <NavLink
+          to={SHOP_ROUTE}
+          onClick={onClose}
+          className={styles.menuLink}
+        >
+          <FaCartShopping /> Магазин
+        </NavLink>
+        <NavLink
+          to={PRIVACY_ROUTE}
+          onClick={onClose}
+          className={styles.menuLink}
+        >
+          <FaUsers /> Реферальная программа
+        </NavLink>
+        <NavLink
+          to={TERMS_ROUTE}
+          onClick={onClose}
+          className={styles.menuLink}
+        >
+          <IoShieldCheckmarkSharp /> Условия гарантии
+        </NavLink>
+        <a
+          href="https://t.me/express_model_marketplace"
+          onClick={onClose}
+          className={styles.menuLink}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaTelegram /> Мы в Telegram
+        </a>
+      </div>
+    </Drawer>
+  );
 };
 
 export default Menu;
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,147 +1,87 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Context } from '../../index';
-import Button from 'react-bootstrap/Button';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 import { observer } from 'mobx-react-lite';
 import { useNavigate } from 'react-router-dom';
-import { FaShoppingCart, FaUserAlt  } from 'react-icons/fa';
-import { LOGIN_ROUTE, ADMIN_ROUTE, BASKET_ROUTE, USER_ACCOUNT_ROUTE, SHOP_ROUTE, PRIVACY_ROUTE, TERMS_ROUTE, SELLER_ACCOUNT_ROUTE } from '../../utils/consts';
-import './NavBar.css';
-import { FaTelegram } from "react-icons/fa6";
+import { IoCart } from 'react-icons/io5';
+import { AiFillDollarCircle } from 'react-icons/ai';
+import { PiListBold } from 'react-icons/pi';
 import EMM from '../../icons/EMM2.png';
-import { IoShieldCheckmarkSharp } from "react-icons/io5";
-import { FaCartShopping } from "react-icons/fa6";
-import { FaUserGroup } from "react-icons/fa6";
 import Menu from '../Menu/Menu';
+import { BASKET_ROUTE, SELLER_ACCOUNT_ROUTE, SHOP_ROUTE } from '../../utils/consts';
+import './NavBar.css';
 
 const NavBar = observer(() => {
-    const { user, thing } = useContext(Context);
-    const [drawerOpen, setDrawerOpen] = useState(false);
-    const navigate = useNavigate();
+  const { user, thing } = useContext(Context);
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    if (user.isAuth) {
+      thing.loadBasket();
+    }
+  }, [user.isAuth, thing]);
 
-    useEffect(() => {
-        if (user.isAuth) {
-            thing.loadBasket(); 
-        }
-    }, [user.isAuth, thing]);
+  return (
+    <div className="navbar">
+      <div className="navbar-content">
+        <div
+          className="navbar-brand"
+          onClick={() => navigate(SHOP_ROUTE)}
+          style={{ cursor: 'pointer'}}
+        >
+          <img src={EMM} alt="EMM" />
+        </div>
 
-    const logOut = () => {
-        user.logout(); 
-        if (window.Telegram?.WebApp?.HapticFeedback) {
-            window.Telegram.WebApp.HapticFeedback.impactOccurred('heavy');
-          }
-        navigate(LOGIN_ROUTE); 
-    };
-
-    return (
-        <Navbar bg="dark" variant="dark" expand={false} className="bg-body-tertiary" style={{ justifyContent: 'center' }}>
-            <div className='navbar'>
-                <Navbar.Brand onClick={() => navigate(SHOP_ROUTE)} style={{color: 'white'}}>
-                    <img src={EMM} alt='EMM' />
-                </Navbar.Brand>
-                
-                <div className='menu'>
-                    {user.isAuth && (
-                        <>
-                        {user.user.role === 'SELLER' && (
-                            <Button
-                                className='seller-account'
-                                variant="outline-light"
-                                onClick={() => navigate(SELLER_ACCOUNT_ROUTE)}
-                            >
-                                <FaUserGroup />
-                            </Button>
-                        )}
-                            <Button
-                                className='shopping-cart'
-                                variant="outline-light"
-                                onClick={() => navigate(BASKET_ROUTE)}
-                                style={{ position: 'relative' }}
-                            >
-                                <FaShoppingCart />
-                                {thing.basket.length > 0 && (
-                                    <span
-                                        style={{
-                                            position: 'absolute',
-                                            top: '-8px',
-                                            right: '-8px',
-                                            backgroundColor: '#d70d0d',
-                                            color: 'white',
-                                            borderRadius: '50%',
-                                            padding: '3px 6px',
-                                            fontSize: '12px',
-                                            fontWeight: 'bold',
-                                            lineHeight: '1',
-                                        }}
-                                    >
-                                        {thing.basket.length}
-                                    </span>
-                                    
-                                )}
-                            </Button>
-                            <Button
-                                className='user-account'
-                                variant="outline-light"
-                                onClick={() => setDrawerOpen(true)}
-                                // onClick={() => navigate(USER_ACCOUNT_ROUTE)}
-                            >
-                                <FaUserAlt />
-                            </Button>
-                        </>
-                    )}
-                    <Navbar.Toggle aria-controls="navbarScroll" data-bs-theme="dark"  style={{color: 'white'}}/>
-                </div>
-                
-                <Navbar.Collapse id="navbarScroll">
-                    <Nav
-                        className="me-auto my-2 my-lg-0"
-                        style={{ maxHeight: '165px', color: 'white' }}
-                        navbarScroll
-                    >
-                        <Nav.Link href='https://t.me/express_model_marketplace' style={{ display: 'flex', gap: '5px', alignItems: 'center', color: 'white' }}>
-                            <FaTelegram /> Us in Telegram
-                        </Nav.Link>
-                        <Nav.Link onClick={() => navigate(SHOP_ROUTE)} style={{ display: 'flex', gap: '5px', alignItems: 'center', color: 'white' }}>
-                            <FaCartShopping /> Store
-                        </Nav.Link>
-                        <Nav.Link onClick={() => navigate(PRIVACY_ROUTE)} style={{ display: 'flex', gap: '5px', alignItems: 'center', color: 'white' }}>
-                            <FaUserGroup /> Referral program
-                        </Nav.Link>
-                        <Nav.Link onClick={() => navigate(TERMS_ROUTE)} style={{ display: 'flex', gap: '5px', alignItems: 'center', color: 'white' }}>
-                            <IoShieldCheckmarkSharp /> Terms of warranty
-                        </Nav.Link>
-                    </Nav>
-                    <div className="d-flex" style={{ justifyContent: 'flex-end' }}>
-                        {user.isAuth ? (
-                            <>
-                                {user.user.role === 'ADMIN' && (
-                                    <Button
-                                        variant="outline-danger"
-                                        style={{ marginRight: '12px' }}
-                                        onClick={() => navigate(ADMIN_ROUTE)}
-                                    >
-                                        Админка
-                                    </Button>
-                                )}
-                                <Button 
-                                    variant="outline-danger" 
-                                    onClick={logOut}>
-                                    Logout
-                                </Button>
-                            </>
-                        ) : (
-                            <Button variant="outline-danger" onClick={() => navigate(LOGIN_ROUTE)}>
-                                Authorization
-                            </Button>
-                        )}
-                    </div>
-                </Navbar.Collapse>
-            </div>
-            <Menu open={drawerOpen} onClose={() => setDrawerOpen(false)} />
-        </Navbar>
-    );
+        <div className="menu">
+          {user.isAuth && (
+            <>
+              {user.user.role === 'SELLER' && (
+                <button
+                  className="navbar-button"
+                  onClick={() => navigate(SELLER_ACCOUNT_ROUTE)}
+                >
+                    <AiFillDollarCircle />
+                </button>
+              )}
+              <button
+                className="navbar-button"
+                onClick={() => navigate(BASKET_ROUTE)}
+                style={{ position: 'relative' }}
+              >
+                <IoCart />
+                {thing.basket.length > 0 && (
+                  <span
+                    style={{
+                      position: 'absolute',
+                      top: '-8px',
+                      right: '-8px',
+                      backgroundColor: '#d70d0d',
+                      color: 'white',
+                      borderRadius: '50%',
+                      padding: '3px 6px',
+                      fontSize: '12px',
+                      fontWeight: 'bold',
+                      lineHeight: '1',
+                    }}
+                  >
+                    {thing.basket.length}
+                  </span>
+                )}
+              </button>
+              <button
+                className="navbar-button"
+                onClick={() => setDrawerOpen(true)}
+              >
+                <PiListBold />
+              </button>
+            </>
+          )}
+        </div>
+      </div>
+      {/* Используем ваш существующий компонент Menu */}
+      <Menu open={drawerOpen} onClose={() => setDrawerOpen(false)} />
+    </div>
+  );
 });
 
 export default NavBar;
